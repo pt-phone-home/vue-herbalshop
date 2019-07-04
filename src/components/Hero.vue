a584203cbaacab6ed7d987a48d7de3d566706622
Peter Tiernan <ptiernan@gmail.com>
2019-07-04 14:09:06 +0100

    updating cart on mobile view

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-full h-30r relative">
-        <img src="https://res.cloudinary.com/www-rocketchipwebsolutions-ie/image/upload/c_scale,h_563/v1562080112/495802-PH7H16-510_tirgph.jpg" alt="" class="w-full h-30r object-cover object-top order-2">
+    <div class="w-full h-64 sm:h-30r relative">
+        <img src="https://res.cloudinary.com/www-rocketchipwebsolutions-ie/image/upload/c_scale,h_563/v1562080112/495802-PH7H16-510_tirgph.jpg" alt="" class="w-full h-64 sm:h-30r object-fit object-top order-2">
         <p class="absolute bottom-0 right-0 pr-24 pb-12">
             <a href="#shop" v-scroll-to="'#shop'" class="bg-green-400 hover:bg-green-500 text-white hover:text-grey-600 active:no-outline text-xl px-4 rounded-lg py-2">Shop Now</a>
         </p>
